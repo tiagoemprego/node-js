a7675396b1d775738bf630e9e8c708acd19cb168
Tiago Marques <tiago.local>
2019-03-22 10:51:30 -0300

    criacao modulo server

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
+var app = require('./config/server');
 
 app.get("/", function (req, res) {
     res.render("home/index");
@@ -16,5 +13,5 @@ app.get("/noticias", function (req, res) {
 });
 
 app.listen(3000, function () {
-   console.log("servidor rodando com o Express")
+   console.log("servidor ON!");
 });
