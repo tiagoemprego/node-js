5a11e98e09d9bed309dbada29b01878d1f6e5c87
Tiago Marques <tiagomarques@Mac-mini-de-Tiago.local>
2019-04-10 14:58:41 -0300

    implementacao conexao com banco

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,18 @@
 module.exports = function(app)
 {
     app.get("/noticias", function (req, res) {
-        res.render("noticias/noticias")
+        var mysql = require('mysql');
+        var conection = mysql.createConnection({
+            host: 'localhost',
+            usuario: 'root',
+            password: 'cat12345',
+            database: 'portal_noticias'
+        });
+
+        conection.query('select * from noticias', function (error,result) {
+           res.send(result)
+        });
+
+        // res.render("noticias/noticias")
     });
-}
+};
