138da8dc035d566e7139a97d50da1a36c3cd3390
Tiago Marques <tiagomarques@Mac-mini-de-Tiago.local>
2019-04-23 13:35:42 -0300

    inicio das rotas com o cosign

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias')(app);
-
-var rotaHome = require('./app/routes/home')(app);
-
-var rotaFormNoticias = require('./app/routes/formulario_inclusao_noticia')(app);
+// #Uso das rotas esta sendo feita com o Cosign #//
+// var rotaNoticias = require('./app/routes/noticias')(app);
+// var rotaHome = require('./app/routes/home')(app);
+// var rotaFormNoticias = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function () {
    console.log("servidor ON!");
