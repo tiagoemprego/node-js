464909952cde455192f125183095b38ebc412589
Tiago Marques <tiagomarques@Mac-mini-de-Tiago.local>
2019-04-25 18:00:52 -0300

    implementacao dos campos do formulario

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -10,12 +10,32 @@ module.exports = function(app)
     {
         var noticia = req.body;
 
+        console.log(noticia);
+
+        req.assert('titulo','Titulo é obrigatorio!').notEmpty();
+        req.assert('resumo','Resumo é obrigatorio!').notEmpty();
+        req.assert('resumo','Nome deve conter entre 10 e 100 caracteres!').len(10, 100);
+        req.assert('autor','Resumo é obrigatorio!').notEmpty();
+        req.assert('data_notici','Data é obrigatorio!').notEmpty();
+        req.assert('noticia','Noticia é obrigatorio!').notEmpty();
+
+        var erros =  req.validationErrors();
+
+        if (erros){
+            res.render("admin/form_add_noticia", {
+                isError: true
+            });
+            return
+        }
+
         var connection = aplication.config.dbConnection();
         var noticiaModel = new aplication.app.models.NoticiasDAO(connection);
 
         //# o getNoticia esta sendo chamado da pasta de model #//
         noticiaModel.salvarNoticia(noticia,function (error, result)
         {
+            console.log(error);
+            console.log(result);
             res.redirect('/noticias');
         });
 
