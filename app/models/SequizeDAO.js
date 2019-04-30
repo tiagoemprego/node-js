c34cd296697282c90d10a3646889677d4e000aad
Tiago Marques <tiagomarques@Mac-mini-de-Tiago.local>
2019-04-30 17:44:27 -0300

    criacao model de join de tabelas

---
+++ app/models/SequizeDAO.js
@@ -0,0 +1,16 @@
+// const sequelize = require('sequelize');
+//
+// const connection = app.config.dbConnection();
+//
+// const sequelize = new sequelize(connection);
+//
+// const comments = sequelize.define('comments', {
+//     autor_comment: {
+//         type: Sequilize.STRING
+//     },
+//     comment: {
+//         type: Sequilize.TEXT
+//     }
+// });
+//
+// // comments.sync({force: true});
