fc398df2827acdee2c5e5fc4f23899c96c0a74b6
Tiago Marques <tiagomarques@Mac-mini-de-Tiago.local>
2019-05-03 11:01:14 -0300

    remocao de plugins que nao estavam sendo usados

--- config/server.js
+++ config/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
-var sequelize = require('sequelize');
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -11,7 +10,6 @@ app.set('views', './app/views');
 app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
-app.use(sequelize);
 
 consign()
     .include('app/routes')
